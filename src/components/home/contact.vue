d77332ade580c3cdeeb911a2f11d0c40c309423f
Shafi Haque <shafihaque7@gmail.com>
2018-06-07 19:56:03 -0700

    More changes

--- src/components/home/contact.vue
+++ src/components/home/contact.vue
@@ -19,7 +19,8 @@
             }
          },
          mounted () {
-            
+            var theToken = localStorage.getItem('user-token');
+            axios.defaults.headers.common['Authorization'] = theToken
             axios
                .get('http://aws.techthatinterest.com/api/contact')
                .then(response => {
